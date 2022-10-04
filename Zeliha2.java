fb066d3ded677d5c7ef31eb8e3ba1f72b98545a7
zungun1989 <zbezgin1989@gmail.com>
2022-10-04 14:46:26 -0600

    Conflict

--- Zeliha2.java
+++ Zeliha2.java
@@ -2,6 +2,7 @@ public class Zeliha2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World4");
     }
 }
 
