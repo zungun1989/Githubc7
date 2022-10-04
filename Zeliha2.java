db4faee7af952b6f96e92f163253b1d9034aca4c
zungun1989 <114784153+zungun1989@users.noreply.github.com>
2022-10-04 14:44:55 -0600

    Update Zeliha2.java

--- Zeliha2.java
+++ Zeliha2.java
@@ -1,7 +1,8 @@
 public class Zeliha2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
     }
 }
 
